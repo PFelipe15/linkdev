2a35586523c6c033fe6e39112b18c8cede49c9fe
Paulo Felipe <PauloFelipet15@gmail.com>
2022-11-11 12:42:41 -0300

    states var for login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,31 @@
 import "./login.css";
+
+import { useState } from "react";
 import Logo from "../../components/logo";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e){
+    e.preventDefault()
+    console.log(email, password)
+  }
   return (
     <div className="login-container">
       <Logo />
 
-      <form className="form">
-        <input type="email" placeholder="Digite seu email" />
-        <input type="password" placeholder="*********" autoComplete="on" />
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input type="password" placeholder="*********" autoComplete="on" value={password}  onChange={(e) => {
+            setPassword(e.target.value);
+          } }/>
         <button type="submit"> Acessar</button>
       </form>
     </div>
